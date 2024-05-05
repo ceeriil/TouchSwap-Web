b6baf34a019fce64745297907a89cbc4c3b76eaa
Angry Coding <mailidanielifechukwu@gmail.com>
2024-05-05 22:53:40 +0100

    User Type

--- services/db/user.ts
+++ services/db/user.ts
@@ -1,86 +1,81 @@
 import { Result, Schema, db, toResult } from "@/services/db";
+import { Typesaurus } from "typesaurus";
 
 export interface User {
-  creationTimestamp: number;
-  role: string;
-  ens: string;
-  function: string;
-  status?: Status;
-  socialLinks?: SocialLinks;
-  skills: string[];
+  creationTimestamp:Typesaurus.ServerDate;
+  username:string,
+  rank:string,
+  balance:number;
+  touches:number;
+  wallet?:string;
+  social?: SocialLinks
+  online:boolean;
+  lastOnline:Typesaurus.ServerDate
+  lang:string,
+  first?: string
+  last?:string
 }
 
 export interface SocialLinks {
-  telegram: string;
   twitter: string;
-  github: string;
   discord: string;
-  email: string;
-  instagram: string;
-}
-
-export interface Status {
-  text: string;
-  timestamp: number;
-}
-
-export interface BuilderFuntionsStats {
-  name: string;
-  count: number;
-  total: number;
 }
 
 export type UserDoc = Schema["users"]["Doc"];
 export type UserResult = Result<User>;
 
-
 export async function findAllUsers(): Promise<UserResult[]> {
   const usersSnaphot = await db.users.all();
   const users = usersSnaphot.map(user => toResult<User>(user));
-  return users;
+  return users; 
 }
 
+export async function createUser(username:string, id:number, first:string , last:string ): Promise<UserResult> {
+  let userId = db.users.id(`${id}`);
+   await db.users.set(userId,($) => ({ 
+    creationTimestamp: $.serverDate()
+    username,
+    first, 
+    last
+  }))
 
+}
 
-export async function findUser(address: string): Promise<UserResult> {
-  const userSnapshot = await db.users.get(db.users.id(address));
+export async function updateUser( id: string,socialLinks: SocialLinks): Promise<UserResult> {
+  const userSnapshot = await db.users.get(db.users.id(id));
+  // await userSnapshot?.ref?.(() => ({
+  //   status: { text: status, timestamp: Date.now() },
+  //   socialLinks,
+  //   skills,
+  // }));
   return toResult<User>(userSnapshot);
 }
 
-export async function createUser(
-  role: string,
-  ens: string,
-  functionTitle: string,
-  address: string,
-  status?: Status,
-  socialLinks?: SocialLinks,
-  skills?: string[],
-): Promise<UserResult> {
-  const userAddress = db.users.id(address);
-  const ref = await db.users.set(userAddress, () => ({
-    role,
-    ens,
-    function: functionTitle,
-    creationTimestamp: Date.now(),
-    status: status || { text: "Hi i am new here", timestamp: Date.now() },
-    socialLinks: socialLinks,
-    skills: skills || [],
-  }));
-  const userSnapshot = await db.users.get(ref.id);
-  return toResult<User>(userSnapshot);
-}
 
-export async function updateUser(
-  address: string,
-  status: string,
-  socialLinks: SocialLinks,
-  skills: string[],
-): Promise<UserResult> {
-  const userSnapshot = await db.users.get(db.users.id(address));
-  await userSnapshot?.ref?.update(() => ({
-    status: { text: status, timestamp: Date.now() },
-    socialLinks,
-    skills,
-  }));
-  return toResult<User>(userSnapshot);
-}
+
+// export async function findAllUsers(): Promise<UserResult[]> {
+//   const usersSnaphot = await db.users.all();
+//   const users = usersSnaphot.map(user => toResult<User>(user));
+//   return users;
+// }
+
+
+// export async function createUser(
+// ): Promise<UserResult> {
+
+// }
+
+// export async function updateUser(
+//   address: string,
+//   status: string,
+//   socialLinks: SocialLinks,
+//   skills: string[],
+// ): Promise<UserResult> {
+//   const userSnapshot = await db.users.get(db.users.id(address));
+//   await userSnapshot?.ref?.update(() => ({
+//     status: { text: status, timestamp: Date.now() },
+//     socialLinks,
+//     skills,
+//   }));
+//   return toResult<User>(userSnapshot);
+// }
