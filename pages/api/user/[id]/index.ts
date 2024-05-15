bf14c4648ac39c6d9b537ebb8824515311c4836b
Angry Coding <mailidanielifechukwu@gmail.com>
2024-05-15 22:43:05 +0100

    user id routes and refers

---
+++ pages/api/user/[id]/index.ts
@@ -0,0 +1,34 @@
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { findUser, updateUser } from "@/services/db/user";
+import "@/services/firebase";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    try {
+      const { id } = req.query;
+      const user = await findUser(id as string);
+      return res.status(200).json(user);
+    } catch (error: any) {
+      return res.status(500).json({ message: "Method not allowed" });
+    }
+  }
+
+  if (req.method === "PATCH") {
+    try {
+      const { id } = req.query;
+      const { status, socialLinks } = req.body;
+      console.log(`EDIT /user/${id}`, id);
+      const user = await findUser(id as string);
+      if (user.exist === false) return res.status(404).json({ message: "User not found." });
+      await updateUser(id as string, socialLinks);
+      return res.status(200).end();
+    } catch (error: any) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
+
+  if (req.method === "POST") {
+    return res.status(405).json({ message: "Method not allowed." });
+  }
+}
