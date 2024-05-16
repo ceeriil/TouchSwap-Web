36a25c45bb9bcd9278e0f615d57dc6ed424619c0
Angry Coding <mailidanielifechukwu@gmail.com>
2024-05-16 05:12:33 +0100

    pages

--- pages/quests/index.tsx
+++ pages/quests/index.tsx
@@ -3,13 +3,6 @@ import { QuestScreen } from "@/components/screens/Quest";
 
 export default function Quests() {
   return (
-    <main className="relative h-screen">
-      <div className="h-full">
         <QuestScreen />
-        <div className="container mx-auto px-6">
-          <Menubar />
-        </div>
-      </div>
-    </main>
   );
 }
