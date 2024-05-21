ea39ce965269a37ac848d8fbbfe2d174b3fd7db1
Angry Coding <mailidanielifechukwu@gmail.com>
2024-05-21 22:06:16 +0100

    Persist

--- services/store/store.ts
+++ services/store/store.ts
@@ -1,9 +1,7 @@
-import { Boost } from "../db/boost";
 import { Energy } from "../db/user";
-import { del, get, set } from "idb-keyval";
 import { mountStoreDevtool } from "simple-zustand-devtools";
 import { create } from "zustand";
-import { StateStorage, createJSONStorage, devtools, persist } from "zustand/middleware";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 
 export type TScreens = "badges" | "boost" | "home" | "refs" | "stats" | "quests" | "social" | "wallet";
 
@@ -35,8 +33,10 @@ export type TUser = {
   connectionId: string;
 };
 
-const emptyUser: TUser = {
-  id: 0,
+export const STORE_NAME = "Touch_Swap_Store";
+
+export const emptyUser: TUser = {
+  id: -1,
   username: "",
   first: "",
   last: "",
@@ -52,6 +52,8 @@ const emptyUser: TUser = {
 };
 
 type TAppStore = {
+  rechargeSpeed: number;
+  defaultData: boolean;
   extraTap: boolean;
   freeBoosts: TBoost[];
   paidBoosts: TBoost[];
@@ -66,27 +68,18 @@ type TAppStore = {
   setPaidBoosts: (boostFields: TBoost[]) => void;
   setFreeBoosts: (boostFields: TBoost[]) => void;
   useRefill: () => void;
-};
-
-const storage: StateStorage = {
-  getItem: async (name: string): Promise<string | null> => {
-    let data = await get(name);
-    return (await get(name)) || Promise.reject(null);
-  },
-  setItem: async (name: string, value: string): Promise<void> => {
-    console.log(name, "with value", value, "has been saved");
-    await set(name, value);
-  },
-  removeItem: async (name: string): Promise<void> => {
-    console.log(name, "has been deleted");
-    await del(name);
-  },
+  updateDefaultData: () => void;
+  increaseMaxEnergy: () => void;
+  increaseTap: () => void;
+  updateEnergyByTime: () => void;
 };
 
 export const useAppStore = create<TAppStore>()(
   devtools(
     persist(
       (set, get) => ({
+        rechargeSpeed: 0,
+        defaultData: true,
         extraTap: false,
         freeBoosts: [],
         paidBoosts: [],
@@ -121,16 +114,19 @@ export const useAppStore = create<TAppStore>()(
               ...user,
               energy: {
                 ...user.energy,
-                energyLeft: user.energy.maxEnergy,
+                energyLeft: user.energy.maxEnergy , // Reset current energy to new max energy
               },
             },
           }));
         },
-        updatePaidBoostLevel: (boostId: number, newLevel: number): void => {
+        updatePaidBoostLevel: (boostId: number): void => {
           const { paidBoosts } = get();
-          const updatedBoosts = paidBoosts.map(boost =>
-            boost.boostId === boostId ? { ...boost, level: newLevel } : boost,
-          );
+          const updatedBoosts = paidBoosts.map(boost => {
+            if(boost.boostId == boostId){
+              return  {...boost, level: boost.level!+1, cost: Math.imul(boost.cost!,2)  }
+            }
+            return boost
+        });
           set(() => ({ paidBoosts: updatedBoosts }));
         },
         useEnergy: (amount: number): void => {
@@ -159,20 +155,61 @@ export const useAppStore = create<TAppStore>()(
         },
         setPaidBoosts: (boosts: TBoost[]): void => set(() => ({ paidBoosts: boosts })),
         setFreeBoosts: (boosts: TBoost[]): void => set(() => ({ freeBoosts: boosts })),
+        updateDefaultData: (): void => set(() => ({ defaultData: false })),
+        increaseMaxEnergy: (): void => {
+          const { user } = get();
+          set(() => ({
+            user: {
+              ...user,
+              energy: {
+                ...user.energy,
+                maxEnergy: user.energy.maxEnergy + 500,
+              },
+            },
+          }));
+        },
+        increaseTap: (): void => {
+          const { user } = get();
+          set(() => ({
+            user: {
+              ...user,
+              tapValue: user.tapValue + 1,
+            },
+          }));
+        },
+        updateEnergyByTime: (): void => {
+          const { user, paidBoosts } = get();
+
+          if (user.energy.energyLeft === user.energy.maxEnergy) return;
+          const boostLevel = (paidBoosts.find(boost => boost.boostId === 3)?.level!) + 1; 
+          let newEnergyLeft = user.energy.energyLeft + boostLevel;
+
+          if (newEnergyLeft > user.energy.maxEnergy) {
+            newEnergyLeft = user.energy.maxEnergy;
+          }
+
+          set(() => ({
+            user: {
+              ...user,
+              energy: {
+                ...user.energy,
+                energyLeft: newEnergyLeft,
+              },
+            },
+          }));
+        },
       }),
       {
-        name: "AppStore",
+        name: STORE_NAME,
         storage: createJSONStorage(() => localStorage),
         onRehydrateStorage: state => {
-          let previousState = localStorage.getItem("AppStore");
+          let previousState = localStorage.getItem(STORE_NAME);
           let defaultStateString = JSON.stringify({ state, version: 0 });
-          console.log("hydration starts", JSON.parse(previousState || ""));
           return (state, error) => {
             if (error) {
-              localStorage.setItem("AppState", defaultStateString);
+              localStorage.setItem(STORE_NAME, defaultStateString);
             } else {
-              console.log(previousState);
-              localStorage.setItem("AppState", previousState || defaultStateString);
+              localStorage.setItem(STORE_NAME, previousState || defaultStateString);
             }
           };
         },
@@ -182,5 +219,5 @@ export const useAppStore = create<TAppStore>()(
 );
 
 if (process.env.NODE_ENV === "development") {
-  mountStoreDevtool("Store", useAppStore);
+  mountStoreDevtool(STORE_NAME, useAppStore);
 }
