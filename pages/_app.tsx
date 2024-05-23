13ce101d296bd36f5ff208ccbd65b136e56234aa
Angry Coding <mailidanielifechukwu@gmail.com>
2024-05-23 05:08:30 +0100

    Fixed Pages  No render if its not a telegram web app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,31 @@
 import type { AppProps } from "next/app";
-import { Menubar } from "@/components/Menubar";
 import "@/styles/globals.css";
-import { WebApp } from "@grammyjs/web-app";
 import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { TelegramWebApps } from 'telegram-webapps-types';
+import useTelegramInitData from "@/hooks/useTelegramInitData";
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isHashValid, setIsHashValid] = useState(false);
+  const initData = useTelegramInitData();
+  useEffect(() => {
+    if(initData.user !== null){
+      axios
+        .post('/api/validateHash', { hash: window.Telegram.WebApp.initData })
+        .then(response => setIsHashValid(response.status === 200))
+        .catch(()=>setIsHashValid(false) );
+      }
+  }, []);
+
+  if (!isHashValid) {
+    return null;
+  }
   return (
     <>
       <Toaster />
-      <main className="relative bg-cover overflow-x-hidden" style={{ background: `url('/img/stars.svg')` }}>
+      <main className="relative bgcover overflowxhidden" style={{ background: `url('/img/stars.svg')` }}>
         <Component {...pageProps} />
       </main>
     </>
