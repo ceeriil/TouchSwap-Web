f8472b4cc100d9105069801cbc94846c775c0b6c
ceeriil <simonorugbo15@gmail.com>
2024-05-24 00:52:11 +0100

    Referral and Bagde Card

--- components/touchswap/quests/ReferralQuest.tsx
+++ components/touchswap/quests/ReferralQuest.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import Image from "next/image";
-import { SpeakerIcon } from "../../assets/SpeakerIcon";
-import { DoubleCoinIcon } from "@/components/assets/DoubleCoinIcon";
+import React, { useState } from "react";
+import { ReferralButton } from "./ReferralButton";
+import { useAppStore } from "@/services/store/store";
 
-type ReferralQuestsList = {
+export type ReferralQuestsList = {
   count: number;
   reward: number;
   claimed?: boolean;
@@ -50,31 +49,17 @@ export const referralQuestsLists: ReferralQuestsList[] = [
 ];
 
 export const ReferralQuests = () => {
+  const totalRefer = useAppStore(state => state.user.totalRefered);
+  const totalRefersCliamed = useAppStore(state => state.user.totalReferedCliamed);
+  const refersUserData = referralQuestsLists.map(refer => {
+    return { ...refer, completed: totalRefer >= refer.count, claimed: totalRefersCliamed >= refer.count };
+  });
+
   return (
     <div className="grid gap-2">
-      {referralQuestsLists.map(({ count, reward, claimed, completed }, index) => {
-        return (
-          <div className="bg-[#293641] py-3 px-4 rounded-[6px] h-full flex items-center justify-between" key={index}>
-            <div className="flex items-center">
-              <Image src={"/img/ref.svg"} width={40} height={40} alt="Referal Icon" />
-              <div className="ml-3">
-                <h3 className="text-[0.8rem] font-[500] leading-[1.8] text-[#AFAFAF]">{count} Invite</h3>
-                <div className="text-[0.75rem] font-medium flex items-center">
-                  <DoubleCoinIcon width="14" height="14" />
-                  <span className="ml-1">{reward}</span>
-                </div>
-              </div>
-            </div>
-            <button
-              className={`${
-                !completed ? "bg-[#A7A7A7]" : "bg-[#FCFCFC]"
-              } text-sm  text-black py-2 px-2 rounded-lg font-medium `}
-            >
-              {claimed ? "claimed" : "claim"}
-            </button>
-          </div>
-        );
-      })}
+      {refersUserData.map(({ count, reward, claimed, completed }, index) => (
+        <ReferralButton count={count} reward={reward} claimed={claimed} completed={completed} key={index} />
+      ))}
     </div>
   );
 };
