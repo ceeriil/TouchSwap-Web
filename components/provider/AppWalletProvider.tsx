f5e49bd313ca4dcd654ecffa2f255710b9bce82b
Angry Coding <mailidanielifechukwu@gmail.com>
2024-06-07 17:06:01 +0100

    App Wallet

--- components/provider/AppWalletProvider.tsx
+++ components/provider/AppWalletProvider.tsx
@@ -1,18 +1,13 @@
 "use client";
  
-import React, { useMemo } from "react";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
-import {
-    UnsafeBurnerWalletAdapter
-} from '@solana/wallet-adapter-wallets';
-
-require("@solana/wallet-adapter-react-ui/styles.css");
+import React, { useMemo, useState, useEffect } from "react";
+import { WalletProvider } from '@tronweb3/tronwallet-adapter-react-hooks';
+import { WalletModalProvider } from '@tronweb3/tronwallet-adapter-react-ui';
+import '@tronweb3/tronwallet-adapter-react-ui/style.css';
+import { LedgerAdapter } from '@tronweb3/tronwallet-adapter-ledger';
+import type { Adapter, WalletError } from '@tronweb3/tronwallet-abstract-adapter';
+import { WalletDisconnectedError, WalletNotFoundError } from '@tronweb3/tronwallet-abstract-adapter';
+import toast from "react-hot-toast";
 
 
 export default function AppWalletProvider({
@@ -20,17 +15,61 @@ export default function AppWalletProvider({
   }: {
     children: React.ReactNode;
   }) {
-    const network = WalletAdapterNetwork.Mainnet;
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-    const wallets = useMemo(() => [
-        new UnsafeBurnerWalletAdapter(),
-    ],[network],);
+
+    function onError(e: WalletError) {
+      if (e instanceof WalletNotFoundError) {
+          toast.error(e.message);
+      } else if (e instanceof WalletDisconnectedError) {
+          toast.error(e.message);
+      } else toast.error(e.message);
+
+    }
+    const [adapters, setAdapters] = useState<Adapter[]>([]);
+    useEffect(() => {
+        import('@tronweb3/tronwallet-adapters').then((res) => {
+            const {
+                BitKeepAdapter,
+                OkxWalletAdapter,
+                TokenPocketAdapter,
+                TronLinkAdapter,
+                WalletConnectAdapter
+            } = res;
+                const tronLinkAdapter = new TronLinkAdapter();
+            const ledger = new LedgerAdapter({
+                accountNumber: 2,
+            });
+            const walletConnectAdapter = new WalletConnectAdapter({
+                network: 'Nile',
+                options: {
+                    relayUrl: 'wss://relay.walletconnect.com',
+                    // example WC app project ID
+                    projectId: 'cea33aaa4be55d92e6c9f3752d377bd6',
+                    metadata: {
+                        name: 'TouchSwap',
+                        description: 'Touch Swap Auth',
+                        url: 'https://web3modal.com', // origin must match your domain & subdomain
+                        icons: ['https://avatars.githubusercontent.com/u/37784886']
+                    },
+                },
+                web3ModalConfig: {
+                    themeMode: 'dark',
+                    themeVariables: {
+                        '--w3m-z-index': '1000',
+                    },
+                },
+            });
+            const bitKeepAdapter = new BitKeepAdapter();
+            const tokenPocketAdapter = new TokenPocketAdapter();
+            const okxwalletAdapter = new OkxWalletAdapter();
+            setAdapters([tronLinkAdapter, bitKeepAdapter, tokenPocketAdapter, okxwalletAdapter, walletConnectAdapter, ledger])
+        });
+    }, [setAdapters])
    
     return (
-      <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider wallets={wallets} autoConnect>
-          <WalletModalProvider>{children}</WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
+      <WalletProvider onError={onError} adapters={adapters} disableAutoConnectOnLoad={true}>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+     </WalletProvider>
     );
   }
