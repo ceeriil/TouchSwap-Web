5d5a2adc81ba6c0aacae7d1246ef60d477d7c76d
Angry Coding <mailidanielifechukwu@gmail.com>
2024-06-22 04:16:24 +0100

    Tron

--- components/screens/ConnectQuest.tsx
+++ components/screens/ConnectQuest.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import { OpenQuestDetailScreen } from "./OpenQuestDetails";
 import { QuestList } from "@/types";
 import { calculateTotalReward } from "@/utils";
-import {
-  WalletActionButton
-} from '@tronweb3/tronwallet-adapter-react-ui';
+import {TonConnectButton} from "@tonconnect/ui-react";
 
 
 export const connectQuestsLists: QuestList = {
   id: "wallet",
   title: "Wallet Connect Fun",
-  desc: "Connect Touchswap to your tron wallet, be careful! Once connected, any rewards would be sent to the connected wallet.",
+  desc: "Connect Touchswap to your ton wallet, be careful! Once connected, any rewards would be sent to the connected wallet.",
   tasks: [
     {
       title: "Connect Wallet",
       completed: false,
       link: "",
       reward: 1000,
-      button:( <WalletActionButton  className="text-sm bg-white text-black py-2 px-2 rounded-lg font-medium"   />),
+      button:( <TonConnectButton  className="text-sm text-black py-2 px-2 rounded-lg font-medium"   />),
     },
   ],
   claimed: false,
@@ -30,7 +28,7 @@ export const ConnectQuestScreen = () => {
   const handleClaim = () => {};
 
   const handleTaskOpen = (index: number) => {
-    
+      console.log(index)
   };
 
   return (
