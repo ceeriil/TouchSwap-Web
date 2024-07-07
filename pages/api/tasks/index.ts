8d71e5fa29d7b80cb8b53371e1eba16d9e3f9187
Angry Coding <mailidanielifechukwu@gmail.com>
2024-07-07 09:48:42 +0100

    Changes

--- pages/api/tasks/index.ts
+++ pages/api/tasks/index.ts
@@ -15,11 +15,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       let parsedData: UserTask[] =  taskes.map(task=>{
         if(foundTaskObject[task.id]) return {...task, reward:task.reward , completed:true}
         return { ...task, completed:false}
-      })
+      }) 
 
       return res.status(200).json(parsedData);
     } catch (error: any) {
-      console.log(error)
       return res.status(500).json({ message: "Method not allowed" });
     }
   }
